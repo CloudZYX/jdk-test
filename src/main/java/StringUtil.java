8d1d08066f2b4bb7cf6884618e2c453d3edbb550
zhouyunxiao <zhouyunxiao@qding.me>
2020-01-13 14:02:52 +0800

    [字符串]运用StringBuilder替换字符串

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,3 +1,6 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 public class StringUtil {
 
     /**
@@ -17,7 +20,7 @@ public class StringUtil {
                 // 转换为小写
                 result.append(lowerChar(c));
             }else {
-                // 保持大写，并下一个字母依然要转换为小写
+                // 转换为大写，并下一个字母依然要转换为小写
                 lowerFlag = true;
                 result.append(upperChar(c));
             }
@@ -42,10 +45,32 @@ public class StringUtil {
         }
     }
 
+    public static String replaceString(String target, int indexStart, int indexEnd, String replaceStr){
+        StringBuilder result = new StringBuilder(target);
+        result.replace(indexStart, indexEnd, replaceStr);
+        return result.toString();
+    }
+
+
     public static void main(String[] args) {
-        String testStr = "ROOM_ID_TesT";
+        String testStr = "STRUCT{ROOM_ID_TesT: 1, ROOM_NamE: \"test_DA\"}";
+        //1. 参数名转换为驼峰命名法
         String resultStr = transStringToHump(testStr);
 
+        //2. 替换部分参数
+//        resultStr  = resultStr.replace("struct" ,"msgContent:");
+        resultStr = replaceString(resultStr, 0, 6, "msgContent:");
         System.out.println("原字符串；" + testStr + "转换为：" + resultStr);
+
+        //3. 处理字符串格式
+        StringBuilder tempStr = new StringBuilder(resultStr);
+        tempStr.append(",msgType: 1");
+        tempStr.insert(0, '{');
+        tempStr.append('}');
+        System.out.println( "转换的对象为：" + tempStr);
+
+        JSONObject messageObj = JSON.parseObject(tempStr.toString());
+        String result = messageObj.getString("msgContent");
+        System.out.println( "提取的对象为：" + result);
     }
 }
